080754224114830bd3878522d96725b219517d49
sty0621 <353269916@qq.com>
2018-07-05 12:15:34 +0800

    a

--- src/main/java/com/niuker/wenda/dao/UserDAO.java
+++ src/main/java/com/niuker/wenda/dao/UserDAO.java
@@ -1,24 +1,29 @@
 package com.niuker.wenda.dao;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.niuker.wenda.model.User;
 
 @Mapper
 public interface UserDAO {
-	String TABEL_NAME = " user ";
-	String INSERT_FIELDS = " name, password, salt, head_url ";
-	String SELECT_FIELDS = " id, " + INSERT_FIELDS;
-	
-	@Insert({"insert into", TABEL_NAME, "(", INSERT_FIELDS, 
-			") values(#{name}, #{password}, #{salt}, #{headUrl})"})
-	int addUser(User user);
-	
-	
-	
-	
-	
-	
-	
+    String TABLE_NAME = "user";
+    String INSET_FIELDS = " name, password, salt, head_url ";
+    String SELECT_FIELDS = " id, name, password, salt, head_url";
+
+    @Insert({"insert into ", TABLE_NAME, "(", INSET_FIELDS,
+            ") values (#{name},#{password},#{salt},#{headUrl})"})
+    int addUser(User user);
+
+    @Select({"select ", SELECT_FIELDS, " from ", TABLE_NAME, " where id=#{id}"})
+    User selectById(int id);
+
+    @Update({"update ", TABLE_NAME, " set password=#{password} where id=#{id}"})
+    void updatePassword(User user);
+
+    @Delete({"delete from ", TABLE_NAME, " where id=#{id}"})
+    void deleteById(int id);
 }
--- src/test/java/com/niuker/wenda/InitDatabaseTests.java
+++ src/test/java/com/niuker/wenda/InitDatabaseTests.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.niuker.wenda.dao.UserDAO;
@@ -14,7 +13,6 @@ import com.niuker.wenda.model.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Sql("/init-schema.sql")
 public class InitDatabaseTests {
 	@Autowired
 	UserDAO userDAO;
